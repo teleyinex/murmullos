18c6ff9f2215cb0d598bc3f4a2d075a492d288f5
Daniel Lombraña González <daniellg@unex.es>
2010-06-27 13:50:19 +0200

    First initial version.
    Murmullos only shows a text message in full-screen.

---
+++ murmullos.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+import sys
+import clutter
+
+class Murmullos:
+    def __init__(self,message):
+        self.stage = clutter.Stage()
+        self.stage.set_color(clutter.color_from_string('Black'))
+        self.stage.set_size(800,600)
+        self.stage.set_title("Murmullos")
+        self.stage.connect('key-press-event',clutter.main_quit)
+        self.stage.set_fullscreen(True)
+
+        color = clutter.Color(0xff,0xff,0xff,0xff)
+
+        self.label = clutter.Text()
+        self.label.set_text(message)
+        self.label.set_color(color)
+        (label_width,label_height) = self.label.get_size()
+        label_x = self.stage.get_width() - label_width - 50
+        label_y = self.stage.get_height() - label_height
+        self.label.set_position(label_x, label_y)
+        self.stage.add(self.label)
+
+
+    def run (self):
+        self.stage.show_all()
+        clutter.main()
+
+
+def main (args):
+    if args:
+        message = args[0]
+    else:
+        message = "Hellow Murmullos"
+    
+    app = Murmullos(message)
+    app.run()
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+
+
