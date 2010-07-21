3d0c872e2b31a21baa3171480a19bab191a71c80
Daniel Lombraña González <daniellg@unex.es>
2010-07-21 13:33:54 +0200

    This version has the Avatar, Dent and the rectangle perfectly centered in the screen, which relies on the resolution of the screen.

--- murmullos.py
+++ murmullos.py
@@ -13,38 +13,61 @@ class Murmullos:
         self.stage.set_size(800,600)
         self.stage.set_title("Murmullos")
         self.stage.connect('key-press-event',clutter.main_quit)
+        self.stage.connect('fullscreen',self.reposition,self)
         self.stage.set_fullscreen(True)
 
         self.color = clutter.Color(0xff,0xff,0xff,0xff)
 
         self.identica = identica.Identica(tag)
 
+        # Code for the bacground rectangle
+        self.rectangle = clutter.Rectangle()
+        self.rectangle.set_border_width(2)
+        self.rectangle.set_color(clutter.Color(0x00,0x00,0x00,0x00))
+        self.rectangle.set_border_color(clutter.Color(0xff,0xff,0xff,0xff))
+        self.rectangle.hide()
+
         # Code for the avatar:
         self.texture = clutter.Texture()
-        self.texture.set_position(300,300)
 
         # Code for the text:
         self.label = clutter.Text()
-        self.label.set_size(600,900)
         self.label.set_color(self.color)
         self.label.set_font_name('Sans 16')
         self.label.set_line_wrap(True)
-        label_x = self.stage.get_width()/2 
-        label_y = self.stage.get_height()/2 
-        self.label.set_position(label_x, label_y)
 
         # Grouping everything:
         self.group = clutter.Group()
-        self.group.add(self.texture,self.label)
+        self.group.add(self.texture,self.label,self.rectangle)
         self.stage.add(self.group)
 
         # Creating the timeline:
-        self.timeline = clutter.Timeline(4000)
+        self.timeline = clutter.Timeline(8000)
         self.timeline.connect('completed',self.on_timeline_completed,self)
         self.timeline.set_loop(True)
         alpha = clutter.Alpha(self.timeline,clutter.LINEAR)
         self.behaviour = clutter.BehaviourOpacity(0xdd,0,alpha)
         self.behaviour.apply(self.group)
+
+    def reposition(stage, frame_num, self):
+        # Obtain the size of the fullscreen stage
+        (x,y) = self.stage.get_size()
+
+        # Size for the group: Rectangle, Texture and Label
+        width = x - (x*40)/100
+        height = y - (y*80)/100
+        self.rectangle.set_size(width, height)
+        self.label.set_size(width-50, height-50)
+
+        # Position for the group: Rectangle, Texture and Label
+        sx = (x/2)-(self.rectangle.get_width()/2)
+        sy = (y/2)-(self.rectangle.get_height()/2)
+
+
+        self.texture.set_position(sx+10,sy+10)
+        self.label.set_position(sx+80,sy+10)
+        self.rectangle.set_position(sx,sy)
+
        
     def IdenticaUpdate(self):
         self.identica.update()
@@ -57,6 +80,7 @@ class Murmullos:
         self.label.set_text(message)
 
     def on_timeline_completed(timeline, frame_num, self):
+        self.rectangle.show()
         item = self.identica.data['results'].pop()
         urllib.urlretrieve(item['profile_image_url'],"avatar")
         self.post("avatar",item['text'])
@@ -69,13 +93,15 @@ class Murmullos:
         self.IdenticaUpdate()
         self.timeline.start()
 
+
+
         clutter.main()
 
 def main (args):
     if args:
         tag = args[0]
     else:
-        tag = "murmullos"
+        tag = "gnu"
     
     app = Murmullos(tag)
     app.run()
